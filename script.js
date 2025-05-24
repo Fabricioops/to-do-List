7fc90aa7a425438fb11514b0fc0e23258a2fb4a5
Fabricio <fabricioztxsantana@gmail.com>
2025-05-23 22:18:32 -0300

    adc organizaçao

---
+++ script.js
@@ -0,0 +1,13 @@
+const taskInput = document.getElementById('task-input');
+const tasklist = document.getElementById('task-list');
+
+function alterartask (){
+
+}
+
+function deletartask(){
+
+}
+function listartask(){
+
+}
